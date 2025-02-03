94079a428ad5239225e390bb082820ea9de7e963
Folami <folami.bidmus@gmail.com>
2025-02-03 17:51:12 +0100

    [init3 commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        while (true) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+            System.out.println(input + ": command not found");
+        }
     }
 }
