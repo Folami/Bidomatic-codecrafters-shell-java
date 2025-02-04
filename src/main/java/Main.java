f32d04ebf9c77007c3a1a2646875a30912dc0907
Folami <folami.bidmus@gmail.com>
2025-02-04 17:41:55 +0100

    [init16 commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,16 +45,11 @@ public class Main {
 
     private static void executeEcho(String[] tokens) {
         if (tokens.length > 1) {
-            String[] processedTokens = new String[tokens.length - 1];
-            for (int i = 1; i < tokens.length; i++) {
-                processedTokens[i - 1] = processEscapeSequences(tokens[i]);
-            }
-            System.out.println(String.join(" ", processedTokens));
+            System.out.println(String.join(" ", Arrays.copyOfRange(tokens, 1, tokens.length)));
         } else {
             System.out.println();
         }
     }
-
 
     private static String[] splitPreservingQuotes(String input) {
         List<String> tokens = new ArrayList<>();
@@ -68,7 +63,7 @@ public class Main {
                 escape = false;
             } else if (c == '\\') {
                 escape = true;
-                currentToken.append(c);
+                // Do NOT append the backslash itself when escaping
             } else if ((c == ' ' || c == '\t') && quoteChar == 0) {
                 if (currentToken.length() > 0) {
                     tokens.add(currentToken.toString());
@@ -76,39 +71,48 @@ public class Main {
                 }
             } else if ((c == '\'' || c == '"') && quoteChar == 0) {
                 quoteChar = c;
-                currentToken.append(c);
+                // Do NOT append the quote character itself when starting a quote
             } else if (c == quoteChar) {
                 quoteChar = 0;
-                currentToken.append(c);
+                // Do NOT append the quote character itself when ending a quote
             } else {
                 currentToken.append(c);
             }
         }
+
         if (currentToken.length() > 0) {
             tokens.add(currentToken.toString());
         }
-        return tokens.toArray(new String[0]);
+
+        String[] tokenArray = tokens.toArray(new String[0]);
+
+        // Process escape sequences AFTER splitting:
+        for (int i = 0; i < tokenArray.length; i++) {
+            tokenArray[i] = processEscapeSequences(tokenArray[i]);
+        }
+
+        return tokenArray;
     }
 
-    private static String processEscapeSequences(String input) {
-        StringBuilder output = new StringBuilder();
-        boolean escape = false;
-        for (char c : input.toCharArray()) {
-            if (escape) {
+    private static String processEscapeSequences(String str) {
+        StringBuilder result = new StringBuilder();
+        boolean escaped = false;
+        for (char c : str.toCharArray()) {
+            if (escaped) {
                 switch (c) {
-                    case 'n': output.append('\n'); break;
-                    case 't': output.append('\t'); break;
-                    case 'r': output.append('\r'); break;
-                    default: output.append(c);
+                    case 'n': result.append('\n'); break;
+                    case 't': result.append('\t'); break;
+                    case 'r': result.append('\r'); break;
+                    default: result.append(c); // Handle other escaped characters
                 }
-                escape = false;
+                escaped = false;
             } else if (c == '\\') {
-                escape = true;
+                escaped = true;
             } else {
-                output.append(c);
+                result.append(c);
             }
         }
-        return output.toString();
+        return result.toString();
     }
 
 
