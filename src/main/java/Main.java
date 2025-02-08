fb06077a27fbe62b2a0335f709fa357f053f3437
Folami <folami.bidmus@gmail.com>
2025-02-08 20:10:58 +0100

    [init17 commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.io.BufferedReader;
 
 public class Main {
     // BUILTINS: A set containing the names of commands that are built into the shell.
