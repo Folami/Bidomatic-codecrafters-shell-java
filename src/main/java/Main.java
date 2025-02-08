150f417c2985b36a6fc128b1b2c485c60ee05296
Folami <folami.bidmus@gmail.com>
2025-02-08 13:01:54 +0100

    [init17 commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,8 @@ public class Main {
 
     private static String promptAndGetInput() {
         System.out.print("$ ");
-        return scanner.hasNextLine() ? scanner.nextLine().trim() : null;
+        return scanner.hasNextLine() 
+            ? scanner.nextLine().trim() : null;
     }
 
     private static void executeCommand(String[] tokens) {
