26309fad41c0e2126b661f87ce2db857b98d3ead
Folami <folami.bidmus@gmail.com>
2025-02-24 18:18:11 +0100

    [init message]

--- src/main/java/Shlex.java
+++ src/main/java/Shlex.java
@@ -137,55 +137,88 @@ public class Main {
     }
 }
 
-class Shlex {
-    public static List<String> split(String s) throws Exception {
+public class Shlex {
+
+    // The states of our tokenizer
+    private enum State {
+        NORMAL,          // Outside of any quotes
+        IN_SINGLE,       // Inside single quotes: everything is literal
+        IN_DOUBLE,       // Inside double quotes: backslashes escape next character
+        ESCAPE           // Next character is escaped
+    }
+
+    /**
+     * Splits the input string into tokens, handling:
+     *   a. Single quotes (contents taken literally; backslashes preserved)
+     *   b. Double quotes (backslashes escape the next character)
+     *   c. Backslashes in unquoted text (escape the next character)
+     *   d. Removing surrounding quotes from quoted executables.
+     *
+     * @param input the command-line input string
+     * @return a list of tokens
+     */
+    public static List<String> split(String input) {
         List<String> tokens = new ArrayList<>();
         StringBuilder current = new StringBuilder();
-        boolean inSingleQuotes = false;
-        boolean inDoubleQuotes = false;
-        boolean escaped = false;
+        State state = State.NORMAL;
+        State prevState = State.NORMAL;  // Used for ESCAPE state
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (escaped) {
-                if (inSingleQuotes) {
-                    current.append('\\').append(c);
-                } else {
-                    if (c == 'n') current.append('\n');
-                    else if (c == 't') current.append('\t');
-                    else if (c == 'r') current.append('\r');
-                    else current.append(c);
-                }
-                escaped = false;
-            } else if (c == '\\') {
-                if (inSingleQuotes) {
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            switch (state) {
+                case NORMAL:
+                    if (c == '\\') {
+                        prevState = state;
+                        state = State.ESCAPE;
+                    } else if (c == '\'') {
+                        // Enter single-quote state; do not include the quote.
+                        state = State.IN_SINGLE;
+                    } else if (c == '"') {
+                        // Enter double-quote state; do not include the quote.
+                        state = State.IN_DOUBLE;
+                    } else if (Character.isWhitespace(c)) {
+                        if (current.length() > 0) {
+                            tokens.add(current.toString());
+                            current.setLength(0);
+                        }
+                    } else {
+                        current.append(c);
+                    }
+                    break;
+                case IN_SINGLE:
+                    // In single quotes, everything is taken literally.
+                    if (c == '\'') {
+                        state = State.NORMAL;
+                    } else {
+                        current.append(c);
+                    }
+                    break;
+                case IN_DOUBLE:
+                    if (c == '\\') {
+                        prevState = state;
+                        state = State.ESCAPE;
+                    } else if (c == '"') {
+                        state = State.NORMAL;
+                    } else {
+                        current.append(c);
+                    }
+                    break;
+                case ESCAPE:
+                    // Append the character regardless of context.
                     current.append(c);
-                } else {
-                    escaped = true;
-                }
-            } else if (c == '\'' && !inDoubleQuotes) {
-                inSingleQuotes = !inSingleQuotes;
-            } else if (c == '"' && !inSingleQuotes) {
-                inDoubleQuotes = !inDoubleQuotes;
-            } else if ((c == ' ' || c == '\t') && !inSingleQuotes && !inDoubleQuotes) {
-                if (current.length() > 0) {
-                    tokens.add(current.toString());
-                    current.setLength(0);
-                }
-            } else {
-                current.append(c);
+                    state = prevState;
+                    break;
             }
         }
-
-        if (escaped || inSingleQuotes || inDoubleQuotes) {
-            throw new Exception("Unmatched quote or escape");
+        if (state == State.ESCAPE) {
+            // Trailing backslash: append it literally.
+            current.append('\\');
         }
-
         if (current.length() > 0) {
             tokens.add(current.toString());
         }
-
         return tokens;
     }
+    
 }
+
